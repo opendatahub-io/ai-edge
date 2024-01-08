b6dc742d8464252b060213ac636ed22190626ab0
Jack Delahunt <jdealhunt@gmail.com>
2024-01-08 15:02:23 +0000

    test: now able to check the status of pipeline runs

--- test/e2e-tests/tests/sanity_test.go
+++ test/e2e-tests/tests/sanity_test.go
@@ -1,69 +1,80 @@
 package tests
 
 import (
-	"github.com/opendatahub-io/ai-edge/test/e2e-tests/oc"
-	support "github.com/opendatahub-io/ai-edge/test/e2e-tests/support"
+	"github.com/opendatahub-io/ai-edge/test/e2e-tests/support"
+	"golang.org/x/net/context"
+	k8sApi "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
+	"time"
 )
 
-const (
-	ContainerImagePipeline    = "../../../pipelines/tekton/build-container-image-pipeline/"
-	ContainerImagePipelineRun = "../../../pipelines/tekton/build-container-image-pipeline/build-container-image-pipelinerun-bike-rentals.yaml"
-)
+func CreateContext() context.Context {
+	return context.Background()
+}
 
-func Test_ConnectingToCluster(t *testing.T) {
-	_, err := support.ClusterClientSet()
+func Test_PipelineExists(t *testing.T) {
+	ctx := CreateContext()
+
+	clients, err := support.CreateClients("test-namespace")
 	if err != nil {
 		t.Fatal(err.Error())
+	}
+
+	list, err := clients.Pipeline.List(ctx, k8sApi.ListOptions{})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	expectedPipelineNames := []string{"build-container-image"}
+	if len(list.Items) != len(expectedPipelineNames) {
+		t.Fatalf("expected %v pipelines but %v existed on the cluster", len(expectedPipelineNames), len(list.Items))
+	}
+
+OUTER:
+	for _, name := range expectedPipelineNames {
+		for _, pipeline := range list.Items {
+			if pipeline.Name == name {
+				continue OUTER
+			}
+		}
+
+		t.Fatalf("no pipeline had the name %v", name)
 	}
 }
 
-func Test_ContainerImagePipelineKustomizeApply(t *testing.T) {
-	clientSet, err := support.ClusterClientSet()
+func Test_PipelineRunsCompleted(t *testing.T) {
+	ctx := CreateContext()
+
+	clients, err := support.CreateClients("test-namespace")
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 
-	ctx := support.CreateContext()
+CheckRuns:
+	for {
+		list, err := clients.PipelineRun.List(ctx, k8sApi.ListOptions{})
+		if err != nil {
+			t.Fatal(err.Error())
+		}
 
-	if false {
-		support.CreateTestNamespace(ctx, clientSet)
-		defer support.DeleteTestNamespace(ctx, clientSet)
-	}
+		for _, pipelineRun := range list.Items {
+			for _, condition := range pipelineRun.Status.Conditions {
+				if condition.Reason == "Failed" {
+					t.Fatalf("pipelineRun \"%v\" failed with message \"%v\"", pipelineRun.Name, condition.Message)
+					return
+				}
 
-	err = oc.Apply(oc.ApplyOptions{
-		Kustomize: ContainerImagePipeline,
-	})
+				if condition.Reason == "Running" {
+					time.Sleep(time.Second * 5)
+					continue CheckRuns // it is still running so restart the loop and wait 5 seconds
+				}
+			}
+		}
 
-	if err != nil {
-		t.Fatal(err.Error())
+		break // none failed and none were still running so all pipeline runs have complete
 	}
 }
 
-func Test_ContainerImagePipelineRuns(t *testing.T) {
-	clientSet, err := support.ClusterClientSet()
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	ctx := support.CreateContext()
-
-	if false {
-		support.CreateTestNamespace(ctx, clientSet)
-		defer support.DeleteTestNamespace(ctx, clientSet)
-	}
-
-	err = oc.Apply(oc.ApplyOptions{
-		Kustomize: ContainerImagePipeline,
-	})
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	err = oc.Create(oc.CreateOptions{
-		Filename: ContainerImagePipelineRun,
-	})
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-}
+// func waitForPipelineRunsToFinish(pipelineRuns *v1.PipelineRunList) error {
+// 	return nil
+// }
